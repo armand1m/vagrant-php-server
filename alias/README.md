10bb3c1f153d442d5b07486c08cc8d3663b655f6
Armando Magalhaes <armando.mag95@gmail.com>
2016-01-29 10:00:31 -0200

    modifying

---
+++ alias/README.md
@@ -0,0 +1,1 @@
+Here you can put some `*.conf` files to include some configuration into your apache server without modifying `httpd.conf`
--- server-conf/bootstrap.sh
+++ server-conf/bootstrap.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
+rpm -Uvh https://mirror.webtatic.com/yum/el6/latest.rpm
+
+# update and upgrade yum
 yum update -y
 yum upgrade -y
 
+# update kernel
 yum install kernel.x86_64 kernel-devel.x86_64 -y
-yum install nano -y
+
+# install apache
 yum install httpd -y
 
-rpm -Uvh https://mirror.webtatic.com/yum/el6/latest.rpm
-yum install php55w php55w-opcache php55w-soap php55w-intl php55w-pecl-xdebug php55w-pgsql php55w-xml php55w-xmlrpc php55w-ldap -y
+# install composer
+curl -sS https://getcomposer.org/installer | php
+
+# install php
+yum install php55w php55w-opcache php55w-soap php55w-intl php55w-pecl-xdebug php55w-pgsql php55w-xml php55w-xmlrpc php55w-ldap php55w-gd -y
